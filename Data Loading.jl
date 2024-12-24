8aee0ba286aba558c597e965d584f56fdd88c0ba
Suhas Cristy Mathey <132161396+Cristy210@users.noreply.github.com>
2024-12-24 11:11:46 -0500

    Code Debugging

--- Data Loading.jl
+++ Data Loading.jl
@@ -30,6 +30,18 @@ Data_dir = joinpath(@__DIR__, "Datasets", "data.csv")
 Data_mat = CSV.read(Data_dir, DataFrame)
 
 # ╔═╡ d331160a-c448-417f-81cc-a5ac8e8bedcc
+patient_ids = Data_mat[:, :id]
+
+# ╔═╡ c37182d1-0c03-4eb5-9211-7de2d8b2914f
+labels = Data_mat[:, :diagnosis]
+
+# ╔═╡ d4a669ca-afaa-43d4-95a1-a44e3711eff5
+feature_names = names(Data_mat)[3:32]
+
+# ╔═╡ e962591d-eb17-4edc-a72c-e5dfda06c8fc
+features_matrix = Matrix(Data_mat[:, feature_names])'
+
+# ╔═╡ e2b96d88-0d65-434b-8aee-6c154d772ac5
 
 
 # ╔═╡ Cell order:
@@ -39,3 +51,7 @@ Data_mat = CSV.read(Data_dir, DataFrame)
 # ╠═b606030d-f326-4ced-a440-8d7d4cad42b9
 # ╠═c5043684-060d-485c-b820-821423f332e1
 # ╠═d331160a-c448-417f-81cc-a5ac8e8bedcc
+# ╠═c37182d1-0c03-4eb5-9211-7de2d8b2914f
+# ╠═d4a669ca-afaa-43d4-95a1-a44e3711eff5
+# ╠═e962591d-eb17-4edc-a72c-e5dfda06c8fc
+# ╠═e2b96d88-0d65-434b-8aee-6c154d772ac5
